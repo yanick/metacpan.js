c0355f98b475baee6c2b6c890e6a3cc63ae5afde
Yanick Champoux <yanick@babyl.dyndns.org>
2013-04-15 20:52:52 -0400

    move urls to use the v0 subspace

--- src/metacpan.js
+++ src/metacpan.js
@@ -5,24 +5,32 @@
 
         self.author = function(pause_id) {
             return $.ajax({
-                url: 'http://api.metacpan.org/author/' + pause_id,
+                url: 'http://api.metacpan.org/v0/author/' + pause_id,
                 dataType: 'json'
             });
         };
 
         self.module = function(module_name) {
             return $.ajax({
-                url: 'http://api.metacpan.org/module/' + module_name,
+                url: 'http://api.metacpan.org/v0/module/' + module_name,
                 dataType: 'json'
             });
         };
 
         self.release = function(release_name) {
             return $.ajax({
-                url: 'http://api.metacpan.org/release/' + release_name,
+                url: 'http://api.metacpan.org/v0/release/' + release_name,
                 dataType: 'json'
             });
         };
+
+        self.file = function( author, release, path ) {
+            return $.ajax({
+            url: 'http://api.metacpan.org/v0/file/' 
+                    + author + '/' + release + '/' + path,
+            dataType: 'json'
+        });
+}
 
         return self;
     };
